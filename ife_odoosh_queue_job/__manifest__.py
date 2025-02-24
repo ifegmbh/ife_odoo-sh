e083bb81725fbd91060644b6d8fe811838e26f73
fb-ife <fouzia.jarrari@ife.de>
2025-02-24 12:55:16 +0000

    [MIG][3140]odoosh_queue_job
    Co-authored-by: Niels Göttsch <121107740+ng-ife@users.noreply.github.com>
    Co-authored-by: fouzia <fouzia.jarrari@gmail.com>

---
+++ ife_odoosh_queue_job/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Queue job for odoo.sh",
+    "author": "Pledra,IFE Gesellschaft für Forschung und Entwicklung",
+    "license": "AGPL-3",
+    "website": "https://www.ife.de",
+    "category": "Generic Modules",
+    "version": "18.0.1.0.0",
+    "depends": ["base", "queue_job"],
+}
---
+++ ife_odoosh_queue_job/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This is a migration of the odoosh_queue_job 
+by Pledra Source: https://github.com/pledra/odoo-sh/
